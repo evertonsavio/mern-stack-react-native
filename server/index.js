c387bf4d9eff623aee08eaac4ddb2324e14de63a
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-23 10:29:39 -0300

    :sparkles: rotas e variaveis de ambiente

--- server/index.js
+++ server/index.js
@@ -1,23 +1,38 @@
 const express = require('express');
-
 const app = express();
 
+//////////////////////Variaveis de ambiente////////////////////
+///////////////////////////////////////////////////////////////
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+///////////////////////////Dummy Data//////////////////////////
+///////////////////////////////////////////////////////////////
+const homes = [{id: 1, type: 'apartmento', cidade: 'Taio'}];
+
+/////////////////////////////Rotas/////////////////////////////
+///////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
   res.send('Alo Express!');
 });
 
-app.get('/api', (req, res) => {
-  res.send([
-    {id: 1, name: 'savio'},
-    {id: 2, name: 'everton'},
-    {id: 3, name: 'lucas'},
-    {id: 4, name: 'santos'},
-  ]);
+app.get('/api/listing', (req, res) => {
+  res.send(homes);
 });
 
+app.get('/api/listing/:id', (req, res) => {
+  const imovel_por_id = homes.find(
+    (casa) => casa.id === parseInt(req.params.id)
+  );
+
+  if (!imovel_por_id) {
+    res.status(404).send('Imovel nao encontrado');
+  } else {
+    res.send(imovel_por_id);
+  }
+});
+
+///////////////////////////////////////////////////////////////
 app.listen(port, () => {
   console.log(`Escutando a porta ${port}`);
 });
