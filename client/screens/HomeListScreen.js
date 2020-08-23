ad64c0acaec20ba3615c5295aca94226813ea4bb
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-23 17:42:21 -0300

    :sparkles: floating button

--- client/screens/HomeListScreen.js
+++ client/screens/HomeListScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {FloatingAction} from 'react-native-floating-action';
 import Card from '../components/Card';
 
 const HomeListScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Card></Card>
+      <FloatingAction
+        position="right"
+        onPressItem={() => console.log('Ola')}
+        animated={false}
+        showBackground={false}
+      ></FloatingAction>
     </View>
   );
 };
 
 export default HomeListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({container: {flex: 1}});
+//FloatingAction so vai aparecer se o flex for 1, ocupando todo o espaco vertical
