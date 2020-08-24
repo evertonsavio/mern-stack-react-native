9888e0b6d1d9ba0b7508c51b261a0bf03cfb0f57
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-24 20:10:34 -0300

    :sparkles: store react-redux

--- client/App.js
+++ client/App.js
@@ -2,12 +2,18 @@ import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
-export default function App () {
-  return <AppNavigator />;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
