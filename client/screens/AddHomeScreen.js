7db18a074c87d96069ae0e392c6c99eb86d56081
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-25 14:20:27 -0300

    :sparkles: formulario com formik state managment

--- client/screens/AddHomeScreen.js
+++ client/screens/AddHomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,52 +6,112 @@ import {
   ScrollView,
   TextInput,
   Button,
+  KeyboardAvoidingView,
 } from 'react-native';
 
-const AddHomeScreen = () => {
-  return (
-    <ScrollView>
-      <View style={styles.form}>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Title</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Image URL</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Home Type</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Price</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Year Built</Text>
-          <TextInput style={styles.input} />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Address</Text>
-          <TextInput style={styles.input} multiline />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Description</Text>
-          <TextInput style={styles.input} multiline />
-        </View>
+import {Formik} from 'formik';
 
-        <View style={styles.buttonContainer}>
-          <Button title="Add Home" />
-        </View>
-      </View>
-    </ScrollView>
+const AddHomeScreen = () => {
+  const [title, setTitle] = useState('');
+
+  return (
+    <KeyboardAvoidingView
+      behavior="height"
+      keyboardVerticalOffset={100}
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <Formik
+          initialValues={{
+            title: '',
+            image: '',
+            homeType: '',
+            price: '',
+            yearBuild: '',
+            address: '',
+            description: '',
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {(props) => (
+            <View style={styles.form}>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={props.handleChange('image')}
+                  value={props.values.image}
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Image URL</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={props.handleChange('title')}
+                  value={props.values.title}
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Home Type</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={props.handleChange('homeType')}
+                  value={props.values.homeType}
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Price</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={props.handleChange('price')}
+                  value={props.values.price}
+                  keyboardType="numeric"
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Year Built</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={props.handleChange('yearBuild')}
+                  value={props.values.yearBuild}
+                  keyboardType="number-pad"
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Address</Text>
+                <TextInput
+                  style={styles.input}
+                  multiline
+                  onChangeText={props.handleChange('address')}
+                  value={props.values.address}
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Description</Text>
+                <TextInput
+                  style={styles.input}
+                  multiline
+                  onChangeText={props.handleChange('description')}
+                  value={props.values.description}
+                />
+              </View>
+
+              <View style={styles.buttonContainer}>
+                <Button title="Add Home" onPress={props.handleSubmit} />
+              </View>
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 export default AddHomeScreen;
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   form: {
     margin: 20,
     backgroundColor: '#ffffff',
