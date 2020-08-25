bf189eaf4e564bfb16e567f29df383626483a81a
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-24 21:06:02 -0300

    :boom: fetch data da api node

--- client/redux/actions/houseAction.js
+++ client/redux/actions/houseAction.js
@@ -3,9 +3,13 @@ export const CREATE_HOUSES = 'CREATE_HOUSES';
 
 export const fetchHouses = () => {
   return async (dispatch) => {
+    const result = await fetch('http://172.18.1.17:3000/api/imoveis');
+    const resultData = await result.json();
+    //console.log(resultData);
+
     dispatch({
       type: FETCH_HOUSES,
-      payload: 1,
+      payload: resultData,
     });
   };
 };
--- client/screens/HomeListScreen.js
+++ client/screens/HomeListScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {FloatingAction} from 'react-native-floating-action';
 import Card from '../components/Card';
+import {useDispatch} from 'react-redux';
+
+import * as houseAction from '../redux/actions/houseAction';
 
 const HomeListScreen = (props) => {
   console.log(props.navigation);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(houseAction.fetchHouses());
+  }, [dispatch]);
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
