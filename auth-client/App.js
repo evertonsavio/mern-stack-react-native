f376410d9c0b2c6d36d6746a9148627e8dea041e
Everton Sávio S. Lucas <everluca@outlook.com>
2020-09-06 16:37:59 -0300

    :sparkles: navigation code

--- auth-client/App.js
+++ auth-client/App.js
@@ -2,21 +2,10 @@ import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import LoginScreen from './screens/LoginScreen';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen></LoginScreen>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator></AppNavigator>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
