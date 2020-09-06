dcd37aa6b9adabb8c16d5ca3e5aa3f5d324d7290
Everton Sávio S. Lucas <everluca@outlook.com>
2020-09-06 16:27:05 -0300

    :boom: auth-client-app screens

---
+++ auth-client/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Hello from Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
---
+++ auth-client/screens/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>Hello from register RegisterScreen</Text>
+    </View>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({});
