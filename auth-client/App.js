99bd6c37c2bd0dab0d5ca3c5702ce8c0498cde3f
Everton Sávio S. Lucas <everluca@outlook.com>
2020-09-07 14:24:53 -0300

    :sparkles: store.js react redux

--- auth-client/App.js
+++ auth-client/App.js
@@ -3,9 +3,15 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import AppNavigator from './navigation/AppNavigator';
+import Provider from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
-  return <AppNavigator></AppNavigator>;
+  return (
+    <Provider store={store}>
+      <AppNavigator></AppNavigator>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({});
